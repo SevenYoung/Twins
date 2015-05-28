a79f446f9b35eba81ed1de959101392d6211f7b7
SevenYoung <13929253149@163.com>
2015-05-28 14:59:55 +0800

    remove the iterator of LinkedStack , because LinkedStack is a LinkedList(Iterable), remove the main test function

--- src/cn/young/ds/LinkedStack.java
+++ src/cn/young/ds/LinkedStack.java
@@ -1,69 +1,15 @@
 package cn.young.ds;
 
-import java.util.Iterator;
-import cn.young.ds.LinkedList;
-
 /**
  * Created by SevenYoung on 15-5-27.
  */
-public class LinkedStack<T> extends LinkedList<T> implements Iterable<T>{
-    @Override
-    public Iterator<T> iterator() {
-        return (new LinkedStackIterator<T>());
-    }
+public class LinkedStack<T> extends LinkedList<T> {
+    //LinkedStack is-a LinkedList!
 
-    private class LinkedStackIterator<T> implements Iterator<T> {
-        Node tmp = getFirst();
-        @Override
-        public boolean hasNext() {
-            return tmp != null;
-        }
-
-        @Override
-        public T next() {
-            T res =  (T) tmp.record;
-            tmp = tmp.next;
-            return res;
-        }
-
-        @Override
-        public void remove() {
-
-        }
-    }
-
-    //Push an item to the top of this stack
-    public void push(T item){
-        insertFirst(item);
-    }
+    public void push(T item){insertFirst(item);}
 
     //Peek the top item without removing it
     public T peek(){return peekFirst();}
 
-    public T pop(){
-        return removeFirst();
-    }
-
-//    //For the test
-//    public static void main(String[] args) {
-//        LinkedStack<Double> ls = new LinkedStack<>();
-//        double[] input = {1.0,2.0,3.0,4.0,5.0};
-//        for(double i: input){
-//            ls.push(i);
-//        }
-//        ls.pop();
-//        System.out.printf("size: " + ls.size() + " " +
-//                "IsEmpty: " + ls.isEmpty() + "\n");
-//        for(double i:ls){
-//            System.out.println(i);
-//        }
-//        System.out.printf("size: " + ls.size() + " " +
-//                "IsEmpty: " + ls.isEmpty() + "\n");
-//        ls.pop();
-//        for(double i:ls){
-//            System.out.println(i);
-//        }
-//
-//    }
-
+    public T pop(){return removeFirst();}
 }
