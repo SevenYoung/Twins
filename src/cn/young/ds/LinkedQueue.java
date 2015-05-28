fc1c7c0eea4cf7bb2d971613f96674825125e1bc
SevenYoung <13929253149@163.com>
2015-05-28 14:58:13 +0800

    Add the LinkedQueue.java quickly by inheriting the LinkedList

--- src/cn/young/ds/LinkedQueue.java
+++ src/cn/young/ds/LinkedQueue.java
@@ -3,5 +3,15 @@ package cn.young.ds;
 /**
  * Created by SevenYoung on 15-5-28.
  */
-public class LinkedQueue {
+public class LinkedQueue<T> extends LinkedList<T> {
+    //LinkedQueue is-a LinkedList
+
+    public void enqueue(T item){insertLast(item);}
+
+    public T dequeue(){return removeFirst();}
+
+    public T first(){return peekFirst();}
+
+    public T last(){return peekLast();}
+
 }
